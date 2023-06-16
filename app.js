8e173f503e2973d999eb1adc9d639b76cd815700
itananeev <ivaylotananeev@cybe.eu>
2023-06-17 01:45:10 +0200

    1

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const proxy = require('express-http-proxy');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/authentication', proxy('http://localhost:3001'));
+
+app.use('/friends', proxy('http://localhost:3002'));
+
+app.use('/notifications', proxy('http://localhost:3004'));
+
+app.use('/', (req, res, next) => {
+    return res.status(200).json({ "msg": "Hello from API" })
+});
+
+app.listen(3003, () => {
+    console.log('API gateway is listening to port 3003')
+});
+
+module.exports = app;
