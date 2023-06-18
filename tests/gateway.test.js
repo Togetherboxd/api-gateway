e754b26e02e6d2deee26cfa8ab9d3b34a6f7ad80
itananeev <ivaylotananeev@cybe.eu>
2023-06-18 20:27:05 +0200

    Create gateway.test.js

---
+++ tests/gateway.test.js
@@ -0,0 +1,32 @@
+const request = require('supertest');
+const app = require('../index');
+
+describe('API Gateway', () => {
+  test('should proxy the authentication requests', async () => {
+    const response = await request(app).post('/authentication/register');
+
+    expect(response.statusCode).toBe(200);
+    // Add your expectations for the response body and headers as needed
+  });
+
+  test('should proxy the friends requests', async () => {
+    const response = await request(app).get('/friends/users');
+
+    expect(response.statusCode).toBe(200);
+    // Add your expectations for the response body and headers as needed
+  });
+
+  test('should proxy the notifications requests', async () => {
+    const response = await request(app).post('/notifications/response');
+
+    expect(response.statusCode).toBe(200);
+    // Add your expectations for the response body and headers as needed
+  });
+
+  test('should respond with "Hello from API" for root endpoint', async () => {
+    const response = await request(app).get('/');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ "msg": "Hello from API" });
+  });
+});
